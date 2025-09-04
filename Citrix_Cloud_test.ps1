fbaf0438ada1ae144052128b3bc23c851eabc45c
CA-The-destroyer <126508033+CA-The-destroyer@users.noreply.github.com>
2025-09-04 10:06:35 -0500

    Add files via upload

---
+++ Citrix_Cloud_test.ps1
@@ -0,0 +1,3 @@
+curl -H "Authorization: CwsAuth bearer <token>" -H "Citrix-Cloud-CustomerId: <cust-id>" ^
+     -H "Accept: application/json" ^
+     "https://api.cloud.com/cvad/manage/Machines?$top=1000"
